b80864c3236f2df033113397d581c2471512f880
jakaria53 <jakariachowdhury53@gmail.com>
2026-04-13 22:26:52 +0600

    app,index,main update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,31 @@
-
 import './App.css'
-import Navbar from './components/Navbar'
+import Banner from './components/banner/Banner'
+import MainSection from './components/MainSection/MainSection'
+import FTABanner from './components/banner/Footerbanner'
+import Features from './components/Features/Features'
+import Header from './components/Header/Header'
+import Pricing from './components/Pricing/Pricing'
+import Process from './components/Process/Process'
+import Footer from './components/Footer/Footer'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react'
+
 function App() {
- 
+  const [cartItems, setCartItems] = useState([]);
 
   return (
     <>
-     <Navbar/> 
+      <ToastContainer/>
+    
+      <Header cartCount={cartItems.length}></Header>
+      <Banner/>
+      <Features/>
+      <MainSection cartItems={cartItems} setCartItems={setCartItems}/>
+      <Process/>
+      <Pricing/>
+      <FTABanner/>
+      <Footer/>
     </>
   )
 }
